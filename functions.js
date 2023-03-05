7dd7da4ae3b1d9133240e2f40a4d06826c1eb9f8
L413 <53121093+L413@users.noreply.github.com>
2023-03-04 20:06:15 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,11 @@
+function rainbow() {
+  document.querySelectorAll('*').forEach(function(node) {
+    node.style.cursor = 'url(https://l413.github.io/Cursor-Changer/icons/rainbow.gif), auto';
+});
+}
 
+function mario() {
+  document.querySelectorAll('*').forEach(function(node) {
+    node.style.cursor = 'url(https://l413.github.io/Cursor-Changer/icons/mario.gif), auto';
+});
+}
