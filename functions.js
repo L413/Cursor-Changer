d1d2cd894639c547d2986066642302848ab95d76
L413 <53121093+L413@users.noreply.github.com>
2023-03-04 20:16:50 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,3 +1,7 @@
+function test() {
+alert('test');
+}
+
 function rainbow() {
   document.querySelectorAll('*').forEach(function(node) {
     node.style.cursor = 'url(https://l413.github.io/Cursor-Changer/icons/rainbow.gif), auto';
